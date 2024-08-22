0af4a4747081421a5daf3c7c44f55f45d39f14ab
Dharshini0004 <dharshini18042004@gmail.com>
2024-08-22 16:50:36 +0530

    Add files via upload
    day4

---
+++ Mobile.java
@@ -0,0 +1,29 @@
+package week1.Day4;
+
+public class Mobile {
+
+	public static void main(String[] args) {
+		String mobileName = "SAMSUNG";
+		String mobileModel = "S24";
+		int mobilePrice = 20000;
+		float mobileOffer = 2.0333331f;
+		double mobileSARValue = 1.1888899999;
+		char mobileLogo = 'S';
+		long imeNum = 12347589338l;
+		boolean mobileDisplay = true;
+
+		System.out.println(mobileName);
+		System.out.print(mobileModel);
+		System.out.println(mobilePrice);
+		System.out.println(mobileOffer);
+		System.out.println(mobileDisplay);
+		System.out.println(mobileSARValue);
+		System.out.println(imeNum);
+		System.out.println(mobileLogo);
+
+		System.out.println(mobileName + "," + mobileModel + "," + mobilePrice + "," + mobileDisplay);
+		System.out.println(mobileName + " \n" + mobileModel + "\n" + mobilePrice + "\n" + mobileDisplay);
+
+	}
+
+}
