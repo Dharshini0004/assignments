66a2ecd20df6e21f5d8a2fd5b91652e3cb75e31d
Dharshini0004 <dharshini18042004@gmail.com>
2024-08-22 20:16:32 +0530

    Add files via upload
    day2

---
+++ Car.java
@@ -0,0 +1,18 @@
+package week1.Day2;
+
+public class Car {
+	public void brake()
+	{
+		System.out.println("brake has been applied");
+	}
+    public void soundHorn() {
+    	System.out.println("sound horn has been applied");
+    }
+	public static void main(String[] args)
+	{
+	Car obj=new Car();
+	obj.brake();
+	obj.soundHorn();
+	}
+
+}
