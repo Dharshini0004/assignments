13e574c6d6ef06501c45fa18993a5a5eaf3fb679
Dharshini S <dharshini18042004@gmail.com>
2024-08-26 20:32:19 +0530

    Add files via upload

---
+++ PositiveNumbers.java
@@ -0,0 +1,16 @@
+package week2.day1;
+
+public class PositiveNumbers {
+
+	public static void main(String[] args) {
+		int num = 1;
+		if (num > 0) {
+			System.out.println("the number is positive");
+		} else {
+			System.out.println("the number is negative");
+		}
+
+	}
+
+}
+ //the number is positive
---
+++ PrintOddOrEven.java
@@ -0,0 +1,16 @@
+package week2.day1;
+
+public class PrintOddOrEven {
+
+	public static void main(String[] args) {
+		int num = 12;
+		if (num % 2 == 0) {
+			System.out.println("The Number is Even");
+		} else {
+			System.out.println("The Number is Negative");
+		}
+
+	}
+
+}
+//The Number is Even
